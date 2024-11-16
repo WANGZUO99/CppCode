d94b331bf7e5114304f762d281adde30f16763c0
ArthurWang <wangzuo99995@gmail.com>
2024-11-16 14:42:16 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int addfunc(int a, int b){
+    return a + b;
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << addfunc(1, 2) << std::endl;
+    return 0;
+}
